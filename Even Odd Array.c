da21ba69fa7faa53333631fb2936665040dc2d8b
GarvGupta2005 <garvg310@gmail.com>
2024-11-27 19:12:29 +0530

    Update Even Odd Array.c

--- Even Odd Array.c
+++ Even Odd Array.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
 
+int main() {
+
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for( int i=0; i<n; i++){
+        scanf("%d",&arr[i]);
+    }
+    for(int j=0;j<n;j++)
+    {
+        if(arr[j]%2==0)
+        {
+            arr[j]=0;
+            printf("%d ",arr[j]); 
+        }
+        else
+        {
+            arr[j]=1;
+            printf("%d ",arr[j]); 
+        }
+    }
+            
+  
+}
